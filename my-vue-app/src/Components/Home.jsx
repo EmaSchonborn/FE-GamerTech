87189c3d3ec7b9148f70687908cf495a8d93030e
nicoyabichino <nicoyabichino@gmail.com>
2023-05-22 19:29:43 -0300

    cambios

--- my-vue-app/src/Components/Home.jsx
+++ my-vue-app/src/Components/Home.jsx
@@ -3,13 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { getProducts } from "../Redux/actions";
 import { useEffect } from "react";
-<<<<<<< HEAD
-import ProductDetail from "./ProductDetail"
-import NavBar from "../NavBar";
-=======
-import ProductDetail from "./ProductDetail";
 import CardsContainer from "./CardsContainer";
->>>>>>> f78da3538dc40328879f812b7f20aaedde0e6913
 // import Filter from "../../components/Filter/Filter";
 // import Paginado from "../../components/Paginado/Paginado";
 // import SearchBar from "../../components/SearchBar/SearchBar";
@@ -23,34 +17,6 @@ export default function Home() {
     dispatch(getProducts());
   }, [dispatch]);
 
-<<<<<<< HEAD
-    
-        if(!allProducts.length){
-                return(
-                        <div>
-                            <h1>No hay productos para mostrar!</h1>
-                      </div>
-                    )
-                }else {
-                        return(
-                                <div>
-                                    <NavBar/>
-                                {/* <ul>
-                                {allProducts.map((p) => (
-                                <ProductDetail
-                                id = {p.id}
-                                name = {p.name}
-                                description = {p.description}
-                                price = {p.price}
-                                imgUrl = {p.imgUrl}
-                                />
-                        ))
-                    }
-                </ul> */}
-            </div>
-        )
-    }}
-=======
   if (!allProducts.length) {
     return (
       <div>
@@ -61,20 +27,7 @@ export default function Home() {
     return (
       <div>
         <CardsContainer />
-        {/* <ul>
-          {allProducts.map((p) => (
-            <ProductDetail
-              key={p.id}
-              id={p.id}
-              name={p.name}
-              description={p.description}
-              price={p.price}
-              imgUrl={p.imgUrl}
-            />
-          ))}
-        </ul> */}
       </div>
     );
   }
 }
->>>>>>> f78da3538dc40328879f812b7f20aaedde0e6913
