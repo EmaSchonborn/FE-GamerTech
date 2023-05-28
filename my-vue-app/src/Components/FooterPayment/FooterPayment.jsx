8076147d117ca2c0ce57f1e77de74fc6a0e0ebc2
unknown <guillermooscarn@gmail.com>
2023-05-28 00:39:01 -0300

    cambios

---
+++ my-vue-app/src/Components/FooterPayment/FooterPayment.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+    return (
+      <footer>
+        {/* <div className="footer_logo">
+          <img
+            id="horizontal_logo"
+            alt="image of the logo"
+            src="../img/horizontal_logo.png"
+          />
+        </div> */}
+        <div className="footer_text">
+          <p>Desarrolladores:</p>
+          <p>
+            Aquí se podría colocar info de los desarrolladores
+          </p>
+        </div>
+      </footer>
+    );
+  };
+
+
+export default Footer;
