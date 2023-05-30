f65abb2674b3eb4b1d99366b5c223dc4d9bbc9d9
Salvador Hilares <salvador.hilares@utec.edu.pe>
2023-05-29 23:23:24 -0500

    cambios

--- my-vue-app/src/Components/Home.jsx
+++ my-vue-app/src/Components/Home.jsx
@@ -1,39 +1,6 @@
 import { useDispatch } from "react-redux";
 import { getProducts } from "../Redux/actions";
-<<<<<<< HEAD
 import { useEffect} from "react";
-=======
-import { useEffect } from "react";
-import CardsContainer from "./CardsContainer";
-import SearchBar from "./SearchBar/SearchBar";
-import { useNavigate } from "react-router";
-
-export default function Home() {
-  const navigate = useNavigate();
-  let isAuthenticated = localStorage.getItem("isAuthenticated");
-  const marcaTiempoLogin = localStorage.getItem("marcaTiempoLogin");
-  const marcaTiempoActual = Date.now();
-  const diferenciaTiempo = marcaTiempoActual - marcaTiempoLogin;
-  const minutosTranscurridos = diferenciaTiempo / 60000;
-  console.log(minutosTranscurridos);
-  if (minutosTranscurridos < 10) {
-    console.log("Aún no han pasado 10 minutos");
-  }
-  if (minutosTranscurridos >= 10) {
-    localStorage.setItem("isAuthenticated", false);
-    localStorage.setItem("marcaTiempoLogin", Date.now());
-  }
-  if (isAuthenticated === "false") {
-    alert("Tu sesión ha caducado. Por favor vuelve a iniciar sesión");
-    navigate("/login");
-  }
-
-  const dispatch = useDispatch();
-
-  /* useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]); */
->>>>>>> dev
 
 export default function Home(){
   const dispatch = useDispatch();
