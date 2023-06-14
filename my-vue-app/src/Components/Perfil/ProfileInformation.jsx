79778bd7ce116672995de622398422c55e6be4f5
Ema Schönborn <schonborn13@gmail.com>
2023-06-14 16:10:37 -0300

    corrección bug foto perfil

--- my-vue-app/src/Components/Perfil/ProfileInformation.jsx
+++ my-vue-app/src/Components/Perfil/ProfileInformation.jsx
@@ -27,36 +27,36 @@ const ProfileInformation = ({input, userVerified}) => {
         const result = await uploadFile(file)
         if(!result) throw new Error('Error al subir la imagen')
         dispatch(modifyUsers({
-          'id' : userVerified.id,
-          'name' : userVerified.name,
-          'email' : userVerified.email,
-          'password' : userVerified.password,
-          'isActive' : userVerified.isActive,
-          'isAdmin' : userVerified.isAdmin,
+          'id' : userVerified.user.id,
+          'name' : userVerified.user.name,
+          'email' : userVerified.user.email,
+          'password' : userVerified.user.password,
+          'isActive' : userVerified.user.isActive,
+          'isAdmin' : userVerified.user.isAdmin,
           'imageUrl' : result
         }))
         setFile(null)
       }else if(!file){
           dispatch(modifyUsers({
-          'id' : userVerified.id,
+          'id' : userVerified.user.id,
           'name' : username,
-          'email' : userVerified.email,
-          'password' : userVerified.password,
-          'isActive' : userVerified.isActive,
-          'isAdmin' : userVerified.isAdmin,
-          'imageUrl' : userVerified.imageUrl
+          'email' : userVerified.user.email,
+          'password' : userVerified.user.password,
+          'isActive' : userVerified.user.isActive,
+          'isAdmin' : userVerified.user.isAdmin,
+          'imageUrl' : userVerified.user.imageUrl
         }))
         setChange(!change)
       }else{
         const result = await uploadFile(file)
         if(!result) throw new Error('Error al subir la imagen')
         dispatch(modifyUsers({
-          'id' : userVerified.id,
+          'id' : userVerified.user.id,
           'name' : username,
-          'email' : userVerified.email,
-          'password' : userVerified.password,
-          'isActive' : userVerified.isActive,
-          'isAdmin' : userVerified.isAdmin,
+          'email' : userVerified.user.email,
+          'password' : userVerified.user.password,
+          'isActive' : userVerified.user.isActive,
+          'isAdmin' : userVerified.user.isAdmin,
           'imageUrl' : result
         }))
         setFile(null)
