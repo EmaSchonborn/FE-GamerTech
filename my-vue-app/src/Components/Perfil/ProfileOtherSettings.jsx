84391b9cdd7964415472fa90ad34469405b0049b
Josue Carrera <josuecarrera13@hotmail.com>
2023-06-16 13:21:58 -0500

    Cambios en estilos perfil

--- my-vue-app/src/Components/Perfil/ProfileOtherSettings.jsx
+++ my-vue-app/src/Components/Perfil/ProfileOtherSettings.jsx
@@ -15,58 +15,68 @@ const ProfileOtherSettings = ({ input }) => {
   return (
     <>
       <h1 className={styles.title}>{input}</h1>
-      <div className={styles.legalSection}>
-        <h2 className={styles.titulo} onClick={() => toggleSeccion("anuncios")}>
-          Informacion sobre Anuncios
-        </h2>
-        {seccionActive === "anuncios" && (
-          <div className={styles.contenido}>
-            <p>Informacion sobre anuncios...</p>
-          </div>
-        )}
+      <div className={styles.setting}>
+        <div className={styles.legalSection}>
+          <h2
+            className={styles.titulo}
+            onClick={() => toggleSeccion("anuncios")}
+          >
+            Informacion sobre Anuncios
+          </h2>
+          {seccionActive === "anuncios" && (
+            <div className={styles.contenido}>
+              <p>Informacion sobre anuncios...</p>
+            </div>
+          )}
 
-        <h2 className={styles.titulo} onClick={() => toggleSeccion("cookies")}>
-          Uso de Cookies
-        </h2>
-        {seccionActive === "cookies" && (
-          <div className={styles.contenido}>
-            <p>Informacion sobre el uso de cookies....</p>
-          </div>
-        )}
+          <h2
+            className={styles.titulo}
+            onClick={() => toggleSeccion("cookies")}
+          >
+            Uso de Cookies
+          </h2>
+          {seccionActive === "cookies" && (
+            <div className={styles.contenido}>
+              <p>Informacion sobre el uso de cookies....</p>
+            </div>
+          )}
 
-        <h2
-          className={styles.titulo}
-          onClick={() => toggleSeccion("privacidad")}
-        >
-          Politica de Privacidad
-        </h2>
-        {seccionActive === "privacidad" && (
-          <div className={styles.contenido}>
-            <p>Informacion sobre la politica de privacidad...</p>
-          </div>
-        )}
+          <h2
+            className={styles.titulo}
+            onClick={() => toggleSeccion("privacidad")}
+          >
+            Politica de Privacidad
+          </h2>
+          {seccionActive === "privacidad" && (
+            <div className={styles.contenido}>
+              <p>Informacion sobre la politica de privacidad...</p>
+            </div>
+          )}
 
-        <h2 className={styles.titulo} onClick={() => toggleSeccion("terminos")}>
-          Terminos de servicio
-        </h2>
-        {seccionActive === "terminos" && (
-          <div className={styles.contenido}>
-            <p>Informacion sobre los terminos de servicio...</p>
-          </div>
-        )}
+          <h2
+            className={styles.titulo}
+            onClick={() => toggleSeccion("terminos")}
+          >
+            Terminos de servicio
+          </h2>
+          {seccionActive === "terminos" && (
+            <div className={styles.contenido}>
+              <p>Informacion sobre los terminos de servicio...</p>
+            </div>
+          )}
 
-        <h2 className={styles.titulo} onClick={() => toggleSeccion("legal")}>
-          Avisos legales
-        </h2>
-        {seccionActive === "legal" && (
-          <div className={styles.contenido}>
-            <p>Informacion sobre los Avisos legales...</p>
-          </div>
-        )}
+          <h2 className={styles.titulo} onClick={() => toggleSeccion("legal")}>
+            Avisos legales
+          </h2>
+          {seccionActive === "legal" && (
+            <div className={styles.contenido}>
+              <p>Informacion sobre los Avisos legales...</p>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
 };
 
 export default ProfileOtherSettings;
-
