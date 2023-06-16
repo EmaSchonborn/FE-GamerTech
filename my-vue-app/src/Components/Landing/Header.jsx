6274c8642d92dab732330e2bd1e16d30076bca52
alearra <alejandroarra.devv@gmail.com>
2023-06-15 22:34:37 -0300

    hero, linkslanding, cards home, productdetail, review

--- my-vue-app/src/Components/Landing/Header.jsx
+++ my-vue-app/src/Components/Landing/Header.jsx
@@ -97,16 +97,26 @@ export const Header = () => {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </a>
-                ))}
+              <div className="flex flex-col space-y-2 py-6">
+              {navigation.map((item) => (
+            <React.Fragment key={item.name}>
+              {item.path ? (
+                <Link
+                  to={item.path}
+                  className="text-base font-semibold leading-6 text-[#484848] hover:text-[#E60011]"
+                >
+                  {item.name}
+                </Link>
+              ) : (
+                <a
+                  href={item.href}
+                  className="text-base font-semibold leading-6 text-[#484848] hover:text-[#E60011]"
+                >
+                  {item.name}
+                </a>
+              )}
+            </React.Fragment>
+          ))}
               </div>
               <div className="py-6">
                 <a
